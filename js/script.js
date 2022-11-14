f14780a7cfc879ae0ce6b5fb5ad02feb19ef6f45
Ruma <matteoh8@hotmail.it>
2022-11-14 12:42:24 +0100

    aggiunti codice per vue

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const eleSliderViewer = document.querySelector('.slider-viewer');
+/*const eleSliderViewer = document.querySelector('.slider-viewer');
 const eleSliderThumbs = document.querySelector('.thumbs');
 const eleSliderText = document.querySelector('.slider-text'); //contenitore dei testi
 const eleBtnUp = document.querySelector('.btn-up');
@@ -69,7 +69,6 @@ for (let i = 0; i < images.length; i++) {
 
 
 
-
 const listEleImg = document.querySelectorAll('.slider-img'); 
 const listThumbs = document.querySelectorAll('.thumb-img');
 const listTitle = document.querySelectorAll('.slider-h3');
@@ -80,7 +79,7 @@ let activeIndex = 0;
 document.body.style.backgroundImage = `url('${images[activeIndex].image}')`; //background url da cambiare al click
 document.body.style.backgroundSize = 'cover';
 
-// aggiungere gli event listeners ai due bottoni
+// Bottone giù
 eleBtnDown.addEventListener('click', function () {
 	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
@@ -102,6 +101,7 @@ eleBtnDown.addEventListener('click', function () {
 	document.body.style.backgroundSize = 'cover';
 });
 
+// Bottone su
 eleBtnUp.addEventListener('click', function () {
 	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
@@ -160,7 +160,7 @@ pauseBtn.addEventListener('click', function (){
 		autoplay = goAhead(); //non so che metterci
 		}
 	}
-)
+)*/
 
 //Funzione andare indietro
 /*const reverse = setInterval(reverseFun, 3000);
@@ -197,3 +197,148 @@ reverseBtn.addEventListener('click', function (){
 		}
 	}
 )*/
+
+
+//Codice con Vue
+const arrImages = [
+	{
+		image: '01.webp',
+		title: "Marvel's Spiderman Miles Morale",
+		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+	},
+	{
+		image: '02.webp',
+		title: 'Ratchet & Clank: Rift Apart',
+		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+	},
+	{
+		image: '03.webp',
+		title: 'Fortnite',
+		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+	},
+	{
+		image: '04.webp',
+		title: 'Stray',
+		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+	},
+	{
+		image: '05.webp',
+		title: "Marvel's Avengers",
+		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+	},
+];
+
+const timeSlider = 1 * 1000;
+let direction = 1;
+let activeIndex = 0;
+let idInterval;
+let isAutoplayActive = true;
+
+renderSlider(arrImages);
+startAutoplay();
+
+
+// EVENT LISTENERS
+
+// per disattivare l'autoslider se ci troviamo con il mouse sopra
+document.querySelector('.slider').addEventListener('mouseenter', () => stopAutoplay());
+// per riattivare l'autoslider quando spostiamo il mouse fuori dallo slider
+document.querySelector('.slider').addEventListener('mouseleave', () => {
+	if (isAutoplayActive) {
+		startAutoplay();
+	}
+});
+
+document.querySelector('.btn-invert').addEventListener('click', () => invertDirection());
+
+document.querySelector('.btn-start-stop').addEventListener('click', function() {
+	if (isAutoplayActive) {
+		stopAutoplay();
+		isAutoplayActive = false;
+		this.innerHTML = 'Start';
+	} else {
+		startAutoplay();
+		isAutoplayActive = true;
+		this.innerHTML = 'Stop';
+	}
+});
+
+const listSlides = document.querySelectorAll('.slide');
+
+const listThumbs = document.querySelectorAll('.thumb-img');
+listThumbs.forEach((eleThumb, index) => {
+	eleThumb.addEventListener('click', () => {
+		listSlides[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
+		activeIndex = index;
+		listSlides[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
+		document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+	})
+});
+
+// aggiungere gli event listeners ai due bottoni
+const eleBtnDown = document.querySelector('.btn-down');
+eleBtnDown.addEventListener('click', () => moveSlide(1));
+
+const eleBtnUp = document.querySelector('.btn-up');
+eleBtnUp.addEventListener('click', () => moveSlide(-1));
+
+// FUNCTIONS
+
+function renderSlider(arrImages) {
+	const eleSliderViewer = document.querySelector('.slider-viewer');
+	const eleSliderThumbs = document.querySelector('.thumbs');
+
+	document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+	// creare i tag immagine nell'html
+	for (let i = 0; i < arrImages.length; i++) {
+		const objSlide = arrImages[i];
+		// creare le slide
+		eleSliderThumbs.innerHTML = eleSliderThumbs.innerHTML + `<img src="img/${objSlide.image}" class="thumb-img ${i === 0 ? 'active' : ''}">`;
+		eleSliderViewer.innerHTML += `
+			<div class="slide ${i === 0 ? 'active' : ''}">
+				<img src="img/${objSlide.image}" alt="${objSlide.title}">
+				<div class="text">
+					<h2>${objSlide.title}</h2>
+					<p>${objSlide.text}</p>
+				</div>
+			</div>
+		`
+	}
+}
+
+function startAutoplay() {
+	idInterval = setInterval(() => moveSlide(direction), timeSlider);
+}
+
+function stopAutoplay() {
+	clearInterval(idInterval);
+}
+
+function invertDirection() {
+	direction *= -1; // 1  * -1 = -1; -1 * -1 = 1
+}
+
+function moveSlide(direction) {
+	// togliere la classe active dall'elemento attivo corrente
+	listSlides[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+	if (direction > 0) {
+		activeIndex++;
+		if (activeIndex === listSlides.length) {
+			activeIndex = 0;
+		}
+	} else {
+		if (activeIndex === 0) {
+			activeIndex = listSlides.length;
+		}
+		activeIndex--;
+	}
+
+	// aggiungere la classe active all'elemento successivo
+	listSlides[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+}
